94a110951867877b18a0806db69ae51d6fb40a21
kugua <mateng100@126.com>
2020-02-14 22:23:44 +0800

    111111111111111

--- shell/install.sh
+++ shell/install.sh
@@ -3,7 +3,7 @@
 masters=('192.168.137.10')
 workers=('192.168.137.11' '192.168.137.12' '192.168.137.13')
 for master in masters; do
-    ssh-keygen -R "${master}" ## 清除之前无效登录信息
+    # ssh-keygen -R "${master}" ## 清除之前无效登录信息
     sshpass -p 'huaren830415' scp '/mnt/k8s/shell/init.sh' "root@${master}:/tmp/init.sh"
     sshpass -p 'huaren830415' scp '/mnt/k8s/shell/master.sh' "root@${master}:/tmp/master.sh"
 	sshpass -p 'huaren830415' ssh "root@${master}"
@@ -13,7 +13,7 @@ for master in masters; do
 done
 
 for worker in workers; do
-    ssh-keygen -R "${worker}"
+    # ssh-keygen -R "${worker}"
     sshpass -p 'huaren830415' scp '/mnt/k8s/shell/init.sh' "root@${worker}:/tmp/init.sh"
 	sshpass -p 'huaren830415' ssh "root@${worker}"
 	/tmp/init.sh
