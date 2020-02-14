abfedeb64580cd914a9f9ccf749b7c86be1dabf1
kugua <mateng100@126.com>
2020-02-14 13:39:18 +0800

    1111

--- shell/install.sh
+++ shell/install.sh
@@ -3,11 +3,11 @@ workers = (192.168.137.11 192.168.137.12)
 yum install -y sshpass
 for master in master; do
 	sshpass -p huaren830415 ssh root@${master}
-	/mnt/tool/init.sh
-	/mnt/tool/master.sh ${master}
+	/mnt/k8s/init.sh
+	/mnt/k8s/master.sh ${master}
 done
 
 for worker in workers; do
 	sshpass -p huaren830415 ssh root@${worker}
-	/mnt/tool/init.sh
+	/mnt/k8s/init.sh
 done
